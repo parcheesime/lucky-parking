bfcaca4d39d0c3b6072ec6b62844091ecc052cda
aletia trepte <aletia.trepte@gmail.com>
2022-10-16 04:39:27 -0700

    .

--- src/data/geojson.py
+++ src/data/geojson.py
@@ -4,11 +4,10 @@ from pathlib import Path
 import glob
 import os
 from make_dataset import clean, create_sample
-
 # Load project directory
 PROJECT_DIR = Path(__file__).resolve().parents[2]
-
-
+NEW_PATH= PROJECT_DIR / "data" / "raw"
+os.chdir(NEW_PATH)
 @click.command()
 @click.argument("frac", type=click.FLOAT)
 def main(frac: float):
@@ -16,18 +15,14 @@ def main(frac: float):
     """
     try:
         # Load newest raw data file
-        raw_file_list = glob.glob(PROJECT_DIR / 'data/raw/*.csv')
-        if raw_file_list:
-            RAW_DATA_FILEPATH = max(
-                glob.glob(PROJECT_DIR / 'data/raw/*.csv', key=os.path.getctime))
-            clean(
-                create_sample(RAW_DATA_FILEPATH,
-                              "data/interim", frac),
-                "data/processed",
-                geojson=True,
-            )
+        newest_file = max(list(Path().glob('*.csv')), key=os.path.getctime)
+        print(type(newest_file))
+        clean(
+            create_sample(newest_file,"data/interim", frac),
+            "data/processed",
+            geojson=True,
+        )
     except:
         print('No raw data. Try make data.')
-
 if __name__ == "__main__":
     main()
